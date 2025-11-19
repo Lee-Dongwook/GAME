4312ebbdcb07f8be45e5fd85f42963f2fd744870
Lee-Dongwook <dlehddnr0713@gmail.com>
2025-11-19 13:54:37 +0900

    feat: game1 init

---
+++ Game1.cs
@@ -0,0 +1,58 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace RpgGame
+{
+    public class Game1: Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.PreferredBackBufferHeight = 720;
+        }
+    }
+
+    protected override void Initialize() 
+    {
+        Input.Initialize();
+        SceneManager.Initialize();
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent() 
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        SceneManager.ChangeScene(new TitleScene(Content));
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        Input.Update();
+        SceneManager.Update(gameTime);
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+
+        _spriteBatch.Begin();
+        SceneManager.Draw(_spriteBatch);
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+}
